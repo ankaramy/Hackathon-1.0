a5724b6ef1c15d6b50a2c96d6ab8b4026b065a61
ankaramy <rami.anka@students.iaac.net>
2025-01-16 12:06:41 +0100

    updates

--- Ramy/script.js
+++ Ramy/script.js
@@ -1,6 +1,6 @@
 // Load the PNG image
 const img = new Image();
-img.src = 'icons/ramy8508.jpg'; // Replace with your image path
+img.src = 'Ramy/ramy8508.jpg'; // Replace with your image path
 
 img.onload = () => {
     // Tiling Pattern 1: Simple Grid
